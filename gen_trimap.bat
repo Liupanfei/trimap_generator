a25476de7de5b5509e124fa3a00e4c4d9f836b13
Linton <877682685@qq.com>
2023-10-13 09:56:17 +0800

    Create gen_trimap.bat
    update bat

---
+++ gen_trimap.bat
@@ -0,0 +1,4 @@
+python trimap_module.py --images-dir ./images/practices/ ^
+                        --save-dir ./images/results/ ^
+                        --dilated-size 5 ^
+                        --dilated-epoch 6
